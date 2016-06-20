01905614830a0255e056e0b0747460e73467e274
Arslan Sapashev <sapashev@gmail.com>
2016-06-20 08:07:51 +0500

    Добавил проверку на правильность указания пути

--- src/main/java/com/sapashev/SimpleConcurrentSearch.java
+++ src/main/java/com/sapashev/SimpleConcurrentSearch.java
@@ -1,6 +1,7 @@
 package com.sapashev;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 /**
  * Searches specified text in files. Search could be conducted concurrently.
@@ -18,15 +19,22 @@ public class SimpleConcurrentSearch {
         catch (MissedArgumentException ex){
             //TODO log exception
         }
+        catch (FileNotFoundException ex){
+            //TODO log exception
+            System.out.println("wrong path");
+        }
     }
 
-    private void startProcession(String[] args) throws MissedArgumentException {
+    private void startProcession(String[] args) throws MissedArgumentException, FileNotFoundException {
         if (args.length < 2){
             throw new MissedArgumentException("missed some arguments");
         }
         String startPlace = args[0];
         String textToSearch = args[1];
-        ThreadPool pool = new ThreadPool(10);
+        if(!(new File(startPlace).exists())){
+            throw new FileNotFoundException("no such file");
+        }
+        ThreadPool pool = new ThreadPool(20);
         try{
             pool.start(new File(startPlace),textToSearch);
         }
